83bf000f796ae06d933dbc22a49994ae416fdbe2
veronika_chukhalova <veronika_chukhalova@refinitiv.com>
2022-02-10 22:00:57 +0200

    Initial commit

---
+++ src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -0,0 +1,12 @@
+package io.cucumber.skeleton;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("io/cucumber/skeleton")
+public class RunCucumberTest {
+}
